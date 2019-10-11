c3ee3bf684474349c61cdcf7f739bf576d81dae0
gaoruhao <ruhaogao@gmail.com>
2019-10-11 19:03:29 +0800

    success on target record to file

--- src/generateWaveFile.py
+++ src/generateWaveFile.py
@@ -3,34 +3,38 @@ import struct
 import time
 from nielvis import AnalogInput, Bank, AIChannel, AIRange, AIMode
 
-def readWaveFile(p_file):
-    with wave.open(p_file, 'rb') as wavefile:
-        params = wavefile.getparams()
-        frameInBytes = wavefile.readframes(wavefile.getnframes())
-        print(len(list(frameInBytes)))
-        int_iter = struct.iter_unpack('<i', frameInBytes)
-        intArray = []
-        for int_tuple in int_iter:
-            intArray.append(int_tuple[0])
-            
-        print(len(intArray))
-        return intArray, params
-
 def writeWaveFile(p_file, p_param, p_intArray):
     with wave.open(p_file, 'wb') as wavefile:
         wavefile.setparams(p_param)
-        packFormat = '<' + str(len(p_intArray)) + 'i'
+        packFormat = '<' + str(len(p_intArray)) + 'h'
         dataInBytes = struct.pack(packFormat, *p_intArray);
+        # print('dataInBytes%d' % len(dataInBytes))
         wavefile.writeframes(dataInBytes)
 
+def encodePCM(p_waveform, p_amplitude, p_bitWidth=16):
+    totalRange = p_amplitude * 2
+    maxDigital = 2 ** p_bitWidth
+    
+    pcmResults = []
+    for point in p_waveform:
+        pcmResult = int((float(point) + p_amplitude) * maxDigital / totalRange)
+        pcmResults.append(pcmResult - (2 ** (p_bitWidth - 1)))
+    
+    return pcmResults
+
 def readWaveformFromAI(p_sampleRate, p_sampleSize):
-    ai_bank = Bank.A
+    ai_bank = Bank.B
     ai_channel = AIChannel.AI0
+    ai_channel_2 = AIChannel.AI1
     ai_range = AIRange.PLUS_OR_MINUS_1V
     ai_mode = AIMode.SINGLE_ENDED
     
     value_array = []
     with AnalogInput({'bank': ai_bank,
+                  'channel': ai_channel,
+                  'range': ai_range,
+                  'mode': ai_mode},
+                  {'bank': ai_bank,
                   'channel': ai_channel,
                   'range': ai_range,
                   'mode': ai_mode}) as AI_single_channel:
@@ -50,50 +54,34 @@ def readWaveformFromAI(p_sampleRate, p_sampleSize):
         # stop signal acquisition
         AI_single_channel.stop_continuous_mode()
     
-    return value_array[0][0]
-
-# return int
-def encodePCM(p_waveform, p_amplitude, p_bitWidth=16):
-    totalRange = p_amplitude * 2
-    maxDigital = 2 ** p_bitWidth
-    
-    pcmResults = []
-    for point in p_waveform:
-        pcmResult = int((float(point) + p_amplitude) * maxDigital / totalRange)
-        pcmResults.append(pcmResult)
-    
-    return pcmResults
+    return value_array[0]
 
 def main():
-    # intArray, params = readWaveFile('/home/admin/test123.wav')
-    # print(params.nchannels)
+    duration = 5
+    sampleRate = 44100
+    sampleSize = sampleRate * duration
     
-    # duration = 5
-    # sampleRate = 44100
-    # sampleSize = sampleRate * duration
+    waveforms = readWaveformFromAI(sampleRate, sampleSize)
     
-    # waveform = readWaveformFromAI(sampleRate, sampleSize)
-    # print('waveform size = %d' % len(waveform))
-    # # for i in range(0, 10):
-    # #     print(waveform[i])
+    pcmChannels = []
+    nchannels = len(waveforms)
+    for waveform in waveforms:
+        pcmResults = encodePCM(waveform, 1.0)
+        print('pcmResults size = %d' % len(pcmResults))
+        pcmChannels.append(pcmResults)   
     
-    # with open('/home/admin/rawaudio.txt', 'w') as rawAudioFile:
-    #     for data in waveform:
-    #         rawAudioFile.write("%f\n" % float(data))
+    sampleSize = len(pcmChannels[0])
     
-    waveform = []
-    with open('/home/admin/rawaudio.txt', 'r') as rawAudioFile:
-        lines = rawAudioFile.readlines()
-        for line in lines:
-            point = float(line)
-            waveform.append(point)
-    
-    pcmResults = encodePCM(waveform, 0.5)
-    print('pcmResults size = %d' % len(pcmResults))
-    # for i in range(0, 10):
-    #     print(pcmResults[i])
-    
-    params = (1, 2, 44100, len(pcmResults), 'NONE', 'not compressed')
-    writeWaveFile('/home/admin/output_test123.wav', params, pcmResults)
+    pcmMerged = []
+    if nchannels > 1:
+        for i in range(sampleSize):
+            pcmMerged.append(pcmChannels[0][i])
+            pcmMerged.append(pcmChannels[1][i])
+    else:
+        pcmMerged = pcmChannels[0]
+        
+    params = (nchannels, 2, 44100 / nchannels, sampleSize, 'NONE', 'not compressed')
+    print(params)
+    writeWaveFile('./output_weather.wav', params, pcmMerged)
 
 main()
