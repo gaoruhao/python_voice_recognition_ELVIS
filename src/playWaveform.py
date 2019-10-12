6cb267f35385de407b533507fc97e60e96f7d2c9
gaoruhao <ruhaogao@gmail.com>
2019-10-12 13:14:13 +0800

    Success local 1-channel wave on target

--- src/playWaveform.py
+++ src/playWaveform.py
@@ -18,14 +18,14 @@ def readWaveFile(p_file):
         print(len(intArray))
         return intArray, params
 
-def decodePCM(p_intArray, p_amplitude, p_bitWidth):
+def decodePCM(p_intArray, p_amplitude, p_bitWidth=16):
     totalRange = p_amplitude * 2
     maxDigital = 2 ** p_bitWidth
     
     waveform = []
     for point in p_intArray:
         analogValue = ((point + 2 ** (p_bitWidth - 1)) * totalRange / maxDigital) - p_amplitude
-        waveform.push(analogValue)
+        waveform.append(analogValue)
     
     return waveform
     
@@ -35,13 +35,13 @@ def writeWaveformToAO(p_waveform, p_sampleRate):
     
     with AnalogOutput({'bank': bank,
                    'channel': channel}) as AO_single_channel:
-
         # configure the sample rate and timeout, then starts the signal generation
         timeout = -1
-        AO_single_channel.start_continuous_mode(p_waveform, p_sampleRate, timeout)
-
         print('start to play ...')
-        AO_single_channel.write(p_waveform, p_sampleRate)
+        AO_single_channel.start_continuous_mode([p_waveform[0:200000]], p_sampleRate, timeout)
+
+        AO_single_channel.write([p_waveform[0:200000]], p_sampleRate)
+        AO_single_channel.write([p_waveform[200001:]], p_sampleRate)
         print('stop play')
 
         # stop signal generation
@@ -49,9 +49,11 @@ def writeWaveformToAO(p_waveform, p_sampleRate):
 
 def playWaveform():
     pcmArray, pcmParams = readWaveFile('/home/admin/output_1chan.wav')
+    print(pcmParams)
     
-    waveformToWrite = decodePCM(pcmArray, 3.3, pcmParams.sampwidth)
+    waveformToWrite = decodePCM(pcmArray, 3.3, pcmParams.sampwidth * 8)
     
+    print(len(waveformToWrite))
     writeWaveformToAO(waveformToWrite, pcmParams.framerate)
 
 playWaveform()
