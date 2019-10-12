a34abaf522c599e06aa5b1eecf3ac4c7b77586a9
gaoruhao <ruhaogao@gmail.com>
2019-10-12 14:44:39 +0800

    Update application

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from waveoperation.generateWaveFile_1channel import generateWaveFile
 from voice.findSongName import findSongName
+from nielvis import Bank, AIChannel
 
 wavefile = '/home/admin/app.wav'
 
 def application():
-    generateWaveFile(wavefile)
+    generateWaveFile(wavefile, Bank.B, AIChannel.AI0)
     findSongName(wavefile)
     
 if __name__ == "__main__":
